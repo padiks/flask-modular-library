1cd4008726707b2c9c5412a512588d4ab32661db
padiks <68003199+padiks@users.noreply.github.com>
2025-10-31 20:42:24 +0300

    Delete apps directory

--- apps/routes/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('home', __name__)
-
-@bp.route('/')
-def index():
-    page_title = "ライトノベル図書館"
-    return render_template('home.html', page_title=page_title, show_hero=False)
