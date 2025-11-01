1226f80201a3f7d4be86f26ccbd052887b22dcc0
padiks <68003199+padiks@users.noreply.github.com>
2025-11-01 13:52:06 +0300

    Add files via upload

--- apps/__init__.py
+++ apps/__init__.py
@@ -17,18 +17,22 @@ def create_app():
     # Blueprints
     # ----------------------
     from .routes.home import bp as home_bp
-    from .routes.about import bp as about_bp
-    from .routes.books import bp as books_bp
-    from .routes.tutorials import bp as tutorials_bp
-    from .routes.sitemap import bp as sitemap_bp		
-    from .routes.markdown_render import bp as md_bp
-		
     app.register_blueprint(home_bp)
-    app.register_blueprint(about_bp, url_prefix='/about')    
+		
+    from .routes.about import bp as about_bp
+    app.register_blueprint(about_bp, url_prefix='/about')  
+		
+    from .routes.books import bp as books_bp
     app.register_blueprint(books_bp, url_prefix='/books')
+		
+    from .routes.tutorials import bp as tutorials_bp
     app.register_blueprint(tutorials_bp, url_prefix='/tutorials')
-    app.register_blueprint(sitemap_bp, url_prefix='/sitemap')		
-    app.register_blueprint(md_bp)
+		
+    from .routes.sitemap import bp as sitemap_bp
+    app.register_blueprint(sitemap_bp, url_prefix='/sitemap')	
+		
+    from .routes.markdown_render import bp as md_bp	
+    app.register_blueprint(md_bp)		
 
     from .routes.search import bp as search_bp
     app.register_blueprint(search_bp, url_prefix='/search')
